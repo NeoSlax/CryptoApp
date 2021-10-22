f2f6d3868e44c3962e1aec0c7748f41bec6db221
Pavel Tretyakov <Pave555@ya.ru>
2021-10-22 18:45:20 +0300

    - add CoinViewModel.kt

--- app/src/main/java/com/neoslax/cryptoapp/MainActivity.kt
+++ app/src/main/java/com/neoslax/cryptoapp/MainActivity.kt
@@ -1,33 +1,24 @@
 package com.neoslax.cryptoapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.neoslax.cryptoapp.api.ApiFactory
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.disposables.CompositeDisposable
-import io.reactivex.rxjava3.schedulers.Schedulers
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
 
 class MainActivity : AppCompatActivity() {
 
-    private val compositeDisposable = CompositeDisposable()
+    private lateinit var viewModel:CoinViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val disposable = ApiFactory.apiService.getPriceInfoFullData().subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                Log.d("TEST_COIN_INFO", it.toString())
-            }, {
-                Log.d("TEST_COIN_INFO", it.toString())
-            })
-        compositeDisposable.add(disposable)
-    }
+        viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
+        viewModel.loadData()
+        viewModel.priceList.observe(this, Observer {
+            Log.d("TEST_COIN_INFO", "Success in activity")
+        })
 
-    override fun onDestroy() {
-        super.onDestroy()
-        compositeDisposable.dispose()
     }
 }

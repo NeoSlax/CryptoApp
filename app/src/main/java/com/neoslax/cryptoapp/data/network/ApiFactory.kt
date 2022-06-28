c2469e28dbf4864d8b7b7bf5c446053ea6812cd9
Pavel Tretyakov <mail@pstretyakov.ru>
2022-06-28 15:10:30 +0300

    - add loading and network status

--- app/src/main/java/com/neoslax/cryptoapp/data/network/ApiFactory.kt
+++ app/src/main/java/com/neoslax/cryptoapp/data/network/ApiFactory.kt
@@ -2,6 +2,7 @@ package com.neoslax.cryptoapp.data.network
 
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -9,10 +10,12 @@ object ApiFactory {
 
     private const val BASE_URL = "https://min-api.cryptocompare.com/data/"
 
+    private val httpLogInterceptor = HttpLoggingInterceptor()
 
     private val retrofit = Retrofit.Builder()
 
         .addConverterFactory(GsonConverterFactory.create())
+        .client(OkHttpClient.Builder().addInterceptor(httpLogInterceptor).build())
         .baseUrl(BASE_URL)
         .build()
 
